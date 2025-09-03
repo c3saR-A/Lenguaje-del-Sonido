890edec51d0f51d845304557ec45df34056c6a39
César Andrade <06cesar.leon@gmail.com>
2025-09-03 17:04:49 -0600

    guardo mis cambios locales

--- src/Main.py
+++ src/Main.py
@@ -1,29 +1,31 @@
 import sys
 from PySide6.QtWidgets import QApplication, QMainWindow
-from Main_ui import Ui_MainWindow # Importanción de la interfaz
+from Main_ui import Ui_MainWindow   # Importación de la interfaz
+from audio_manager import AudioManager
 
 class MainWindow(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
+        self.audio_manager = AudioManager()
 
-        # Conecta el botón "Analizar" a una función
+        # Conexión de los botones a la logica principal
         self.btnAnalizar.clicked.connect(self.analizar_sonido)
-
-        # Conecta el boton "Generar Tono" a una función
         self.btnGenerarTono.clicked.connect(self.activar_controles)
-
-        # Conecta el boton "Cancelar Tono" a una función
         self.gbtnCancelarTono.clicked.connect(self.desactivar_controles)
+        self.gbtnReproducir.clicked.connect(self.reproducir_sonido)
+        self.gbtnDetener.clicked.connect(self.detener_sonido)
 
     # todo aplicar Fourier
     def analizar_sonido(self):
         # Aquí va el código para analizar el sonido
         print("Analizando sonido...")
 
+    # Funciones para manejo de interfaz
     def activar_controles(self):
         self.grbSeno.setCheckable(True)
         self.grbCoseno.setCheckable(True)
+        self.grbSeno.setChecked(True)
         self.gbControles.setEnabled(True)
 
     def desactivar_controles(self):
@@ -34,6 +36,20 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         self.gbControles.setEnabled(False)
 
     # todo capturar datos de controles
+    def reproducir_sonido(self):
+        frecuencia: int = self.txtFrecuencia.text()
+        amplitud: int = self.sldAmplitud.value()
+        funcion: str = "Coseno" if self.grbCoseno.isChecked() else "Seno"
+
+        # envio de datos a la clase AudioManager
+        self.audio_manager.reproducir_tono(frecuencia, amplitud, funcion)
+
+    def detener_sonido(self):
+        # todo algo
+        pass
+
+
+
     # todo generar grafica con los datos de controles
     # todo poner grafica en interfaz
 
---
+++ src/audio_manager.py
@@ -0,0 +1,25 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+class AudioManager:
+    def __init__(self):
+        pass
+
+    def reproducir_tono(self, frecuencia: int, amplitud: int, funcion: str):
+        # todo lógica para genera y reproducir onda
+
+        print(f"Datos: \n"
+              f"Fr: {frecuencia} \n"
+              f"Am: {amplitud} \n"
+              f"Fun: {funcion}")
+
+        if funcion == "Seno":
+            x = np.linspace(-np.pi, np.pi, 10)
+            print(f"Onda: {np.sin(x)}")
+        else:
+            onda = np.cos()
+            print(f"Onda: {onda}")
+
+    def detener_tono(self):
+        # todo logica para detener tono
+        pass
